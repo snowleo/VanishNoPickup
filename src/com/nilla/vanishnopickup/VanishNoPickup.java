76cbec69e5ca6642d73be1d3d6d355fdec38f8f3
Chris Cowan <agentme49@gmail.com>
2011-05-22 18:37:28 -0600

    getDistance function didn't actually compare two players.
    Also, if two players are in different worlds, then there's infinite
    distance between them.

--- src/com/nilla/vanishnopickup/VanishNoPickup.java
+++ src/com/nilla/vanishnopickup/VanishNoPickup.java
@@ -328,8 +328,11 @@ public class VanishNoPickup extends JavaPlugin
 
 	public double getDistance(Player player1, Player player2)
 	{
+		if (player1.getWorld() != player2.getWorld())
+			return Double.POSITIVE_INFINITY;
+
 		Location loc1 = player1.getLocation();
-		Location loc2 = player1.getLocation();
+		Location loc2 = player2.getLocation();
 		return Math.sqrt(Math.pow(loc1.getX() - loc2.getX(), 2) + Math.pow(loc1.getY() - loc2.getY(), 2) + Math.pow(loc1.getZ() - loc2.getZ(), 2));
 	}
 
